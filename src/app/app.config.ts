3c67be88cbfbbc0d2b92852e12986dd472f1c16a
rojan.joshi <rojan.joshi@DESKTOP-RNJNJAR>
2023-09-27 00:54:27 +0545

    WildCardRouting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,19 +3,28 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { Routes, provideRouter } from '@angular/router';
 import { GroceryComponent } from './forms/grocery/grocery.component';
 import { CardItemComponent } from './components/card/card-item/card-item.component';
+import { NotfoundComponent } from './forms/notfound/notfound.component';
 
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   //{path: 'sports', component: SportsComponent}, // Eagerly loading route
+
   {path: 'grocery', component: GroceryComponent}, 
+
   { path: 'sports', loadComponent: () => import('./forms/sports/sports.component')
   .then(m => m.SportsComponent) }, // Lazily loading route
+
   { path: 'home', loadComponent: () => import('./forms/home/home.component')
   .then(m => m.HomeComponent) }, // Lazily loading route
-  
+
   { path: 'sports/card-item/:id', component: CardItemComponent},
-  {path: 'grocery', loadChildren: () => import('./forms/grocery/grocery.routes').then(m => m.routes)}
+
+  {path: 'grocery', loadChildren: () => import('./forms/grocery/grocery.routes').then(m => m.routes)},
+
+  {path: 'notfound', component: NotfoundComponent}, 
+
+  {path: '**', component: NotfoundComponent}, 
 ];
 
 export const appConfig: ApplicationConfig = {
---
+++ src/app/forms/notfound/notfound.routes.ts
@@ -0,0 +1,8 @@
+// import { Routes } from "@angular/router";
+// import { NotfoundComponent } from "./notfound.component";
+
+// export const routes: Routes = [
+//     { path: '', redirectTo: '/home', pathMatch: 'full' },
+//     { path: 'notfound', component: NotfoundComponent }
+   
+//   ];
